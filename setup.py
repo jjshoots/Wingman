a1775ea5ff7fb71cab4cb683d62c601e3cc887e5
Jet <taijunjet@hotmail.com>
2022-10-05 15:02:26 +0100

    add gh actions

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="Wingman",
+    version="0.0.1",
+    author="Jet",
+    author_email="taijunjet[at]hotmail[dot]com",
+    description="Wingman for all your AI applications",
+    url="https://github.com/jjshoots/Wingman",
+    license_files=("LICENSE.txt"),
+    long_description="Wingman library for AI projects.",
+    long_description_content_type="text/markdown",
+    keywords=["Machine Learning"],
+    python_requires=">=3.7, <3.11",
+    packages=["wingman"],
+    include_package_data=True,
+    install_requires=[
+        "numpy", "torch", "wandb", "pyyaml"],
+)
