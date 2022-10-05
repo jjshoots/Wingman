8d2f9b066e7909d3c25ef1f674d1bbf6db6a43e6
Jet <taijunjet@hotmail.com>
2022-10-05 16:15:32 +0100

    fix rogue dot

--- wingman/__init__.py
+++ wingman/__init__.py
@@ -1,4 +1,4 @@
-from utensils import cpuize, gpuize, shutdown_handler
+from .utensils import cpuize, gpuize, shutdown_handler
 
 from .neural_blocks import Neural_blocks
 from .wingman import Wingman
