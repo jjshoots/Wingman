bec9b3752858854b839a1af069e707cdb3e51331
Jet <taijunjet@hotmail.com>
2022-10-05 16:23:52 +0100

    isort

--- wingman/__init__.py
+++ wingman/__init__.py
@@ -1,4 +1,3 @@
-from .utensils import cpuize, gpuize, shutdown_handler
-
 from .neural_blocks import Neural_blocks
+from .utensils import cpuize, gpuize, shutdown_handler
 from .wingman import Wingman
