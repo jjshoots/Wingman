a8d6ac5e3a72cbf07c699fffa6b4fed2d570841f
jjshoots <taijunjet@hotmail.com>
2023-05-29 12:18:10 +0100

    better replay buffer test

--- test/test_replay_buffer.py
+++ test/test_replay_buffer.py
@@ -7,6 +7,14 @@ import pytest
 from wingman import ReplayBuffer
 
 
+def is_equivalent_tuple(item1, item2):
+    """Checks whether a two tuples of np.ndarrays are equivalent to each other."""
+    equivalence = True
+    for i1, i2 in zip(item1, item2):
+        equivalence = (i1 == i2).all() and equivalence
+    return equivalence
+
+
 @pytest.mark.parametrize("random_rollover", [True, False])
 def test_bulk(random_rollover):
     """Tests repeatedly bulking the buffer and whether it rollovers correctly."""
@@ -24,17 +32,30 @@ def test_bulk(random_rollover):
             data.append(np.random.randn(bulk_size, *shape))
         memory.push(data, bulk=True, random_rollover=random_rollover)
 
-        # if random rollover and we're more than full, don't care anymore
+        # reverse the data to make indexing for checking easier
+        reversed_data = list(map(list, zip(*data)))
+
+        # if random rollover and we're more than full, different matching technique
         if random_rollover and memory.is_full:
-            return
+            num_matches = 0
+            # match according to meshgrid
+            for item1 in reversed_data:
+                for item2 in memory:
+                    num_matches += int(is_equivalent_tuple(item1, item2))
+
+            assert (
+                num_matches == bulk_size
+            ), f"""Expected {bulk_size} matches inside the memory, got {num_matches}."""
+
+            continue
 
         for step in range(bulk_size):
-            output = memory.__getitem__((iteration * bulk_size + step) % mem_size)
-            for idx in range(len(data)):
-                assert (
-                    output[idx] == data[idx][step]
-                ).all(), f"""Something went wrong with rollover at iteration {iteration},
-                step {step}, expected {data[idx][step]}, got {output[idx]}."""
+            item1 = reversed_data[step]
+            item2 = memory.__getitem__((iteration * bulk_size + step) % mem_size)
+            assert is_equivalent_tuple(
+                item1, item2
+            ), f"""Something went wrong with rollover at iteration {iteration},
+                step {step}, expected {item1}, got {item2}."""
 
 
 @pytest.mark.parametrize("random_rollover", [True, False])
@@ -45,31 +66,42 @@ def test_non_bulk(random_rollover):
     memory = ReplayBuffer(mem_size=mem_size)
 
     previous_data = []
-    for iteration in range(50):
+    for iteration in range(20):
         current_data = []
         for shape in element_shapes:
             current_data.append(np.random.randn(*shape))
         memory.push(current_data, random_rollover=random_rollover)
 
-        # if random rollover and we're more than full, don't care anymore
+        # if random rollover and we're more than full, different matching method
         if random_rollover and memory.is_full:
-            return
+            num_current_matches = 0
+            num_previous_matches = 0
+            for item in memory:
+                num_current_matches += int(is_equivalent_tuple(item, current_data))
+                num_previous_matches += int(is_equivalent_tuple(item, previous_data))
+
+            assert (
+                num_current_matches == 1
+            ), f"""Expected 1 match for current_data, got {num_current_matches}."""
+            assert (
+                num_previous_matches == 1
+            ), f"""Expected 1 match for previous_data, got {num_previous_matches}."""
+
+            continue
 
         # check the current data
         output = memory.__getitem__(iteration % mem_size)
-        for idx in range(len(current_data)):
-            assert (
-                output[idx] == current_data[idx]
-            ).all(), f"""Something went wrong with rollover at iteration {iteration},
-            expected {current_data[idx]}, got {output[idx]}."""
+        assert is_equivalent_tuple(
+            output, current_data
+        ), f"""Something went wrong with rollover at iteration {iteration},
+            expected {current_data}, got {output}."""
 
         # check the previous data
         if iteration > 0:
             output = memory.__getitem__((iteration - 1) % mem_size)
-            for idx in range(len(current_data)):
-                assert (
-                    output[idx] == previous_data[idx]
-                ).all(), f"""Something went wrong with rollover at iteration {iteration},
-                expected {previous_data[idx]}, got {output[idx]}."""
+            assert is_equivalent_tuple(
+                output, previous_data
+            ), f"""Something went wrong with rollover at iteration {iteration},
+                expected {previous_data}, got {output}."""
 
         previous_data = deepcopy(current_data)
