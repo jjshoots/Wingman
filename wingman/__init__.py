6fe31fb8398b8c7398890cabf547a743eacecab9
snow-fox <taijunjet@hotmail.com>
2024-03-12 10:42:11 +0000

    warn for torch

--- wingman/__init__.py
+++ wingman/__init__.py
@@ -1,4 +1,17 @@
 # ruff: noqa: F401
-from .neural_blocks import NeuralBlocks
-from .replay_buffer import ReplayBuffer
 from .wingman import Wingman
+
+try:
+    import torch as _
+
+    from .neural_blocks import NeuralBlocks
+    from .replay_buffer import ReplayBuffer
+except ImportError:
+    import warnings
+
+    warnings.warn(
+        "Could not import torch, "
+        "this is not bundled as part of Wingman and has to be installed manually, "
+        "as a result, `NeuralBlocks` and `ReplayBuffer` are unavailable.",
+        category=RuntimeWarning,
+    )
