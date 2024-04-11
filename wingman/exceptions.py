9f1ebc2069e61f07d23fe0c428ab2b17b6d220b1
Jet <taijunjet@hotmail.com>
2024-04-11 21:42:44 +0100

    add exceptions

---
+++ wingman/exceptions.py
@@ -0,0 +1,44 @@
+from wingman.print_utils import cstr
+
+
+class WingmanException(Exception):
+    """WingmanException."""
+
+    def __init__(self, message: str = ""):
+        """__init__.
+
+        Args:
+            message (str): the message
+        """
+        message = cstr(message, "FAIL")
+        super().__init__(message)
+        self.message = message
+
+
+class NeuralBlocksException(Exception):
+    """NeuralBlocksException."""
+
+    def __init__(self, message: str = ""):
+        """__init__.
+
+        Args:
+            message (str): the message
+        """
+        message = cstr(message, "FAIL")
+        super().__init__(message)
+        self.message = message
+
+
+class ReplayBufferException(Exception):
+    """ReplayBufferException."""
+
+    def __init__(self, message: str = ""):
+        """__init__.
+
+        Args:
+            message (str): the message
+        """
+        message = cstr(message, "FAIL")
+        super().__init__(message)
+        self.message = message
+
